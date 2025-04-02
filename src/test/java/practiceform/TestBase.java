94c22455fde0a06d645f7a9212111db4cd14cd1c
peppbaggio <melkijbes@gmail.com>
2025-04-02 23:54:31 +0300

    Added browser size

--- src/test/java/practiceform/TestBase.java
+++ src/test/java/practiceform/TestBase.java
@@ -14,6 +14,7 @@ public class TestBase {
     @BeforeAll
     static void configSetup() {
         Configuration.browser = "firefox";
+        Configuration.browserSize = "1920x1080";
         Configuration.pageLoadStrategy = "eager";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
