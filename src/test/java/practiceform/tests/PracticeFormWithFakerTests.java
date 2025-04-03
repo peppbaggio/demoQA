96c891cea6cfefcd9a5ed1f6a5e159c194cf621e
peppbaggio <melkijbes@gmail.com>
2025-04-03 14:01:40 +0300

    Changed pictures

--- src/test/java/practiceform/tests/PracticeFormWithFakerTests.java
+++ src/test/java/practiceform/tests/PracticeFormWithFakerTests.java
@@ -1,17 +1,48 @@
 package practiceform.tests;
 
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.*;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Tag;
-import org.junit.jupiter.api.Test;
-import practiceform.TestBase;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+//import practiceform.TestBase;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import practiceform.helpers.Attach;
 import practiceform.page.RegistrationPageSteps;
+
+import java.util.Map;
 
 import static practiceform.data.TestData.*;
 
 
-public class PracticeFormWithFakerTests extends TestBase {
+public class PracticeFormWithFakerTests {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+//        Configuration.browser = "chrome";
+        Configuration.timeout = 10000;
+//        Configuration.holdBrowserOpen = true;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+    }
+
     practiceform.page.RegistrationPageSteps steps = new RegistrationPageSteps();
 
     @Test
