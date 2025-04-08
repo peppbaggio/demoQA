da0ab2adec6721c6c7f6819db747c1eafe1fd26d
peppbaggio <melkijbes@gmail.com>
2025-04-08 22:17:05 +0300

    Added simple test

--- src/test/java/guru/tests/ParameterizedTests.java
+++ src/test/java/guru/tests/ParameterizedTests.java
@@ -4,6 +4,7 @@ import guru.TestBase;
 import guru.data.FakerData;
 import guru.data.Genders;
 import guru.pages.Steps;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
