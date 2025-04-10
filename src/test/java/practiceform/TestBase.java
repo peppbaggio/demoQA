4cd994f53db1103922759a49378a5e3576ceae4b
peppbaggio <melkijbes@gmail.com>
2025-04-10 19:18:28 +0300

    Removed redundant import from TestBase

--- src/test/java/practiceform/TestBase.java
+++ src/test/java/practiceform/TestBase.java
@@ -1,6 +1,5 @@
 package practiceform;
 
-import com.codeborne.selenide.Config;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
